17e8dc780ceeff6da3b16eb99894e7caaef62628
Daniel Vertlib <danielvertlib@hotmail.ca>
2018-03-04 12:18:53 -0800

    added absyn functions for expressions

--- absyn/Absyn.java
+++ absyn/Absyn.java
@@ -43,26 +43,23 @@ abstract public class Absyn {
     }
   }
 
-  static public void showTree( AssignExp tree, int spaces ) {
+  static public void showTree( NilExp tree, int spaces ) {
     indent( spaces );
-    System.out.println( "AssignExp:" );
-    spaces += SPACES;
-    showTree( tree.lhs, spaces );
-    showTree( tree.rhs, spaces );
   }
 
-  static public void showTree( IfExp tree, int spaces ) {
+  static public void showTree( VarExp tree, int spaces ) {
     indent( spaces );
-    System.out.println( "IfExp:" );
-    spaces += SPACES;
-    showTree( tree.test, spaces );
-    showTree( tree.thenpart, spaces );
-    showTree( tree.elsepart, spaces );
+    System.out.println( "VarExp:" );
   }
 
   static public void showTree( IntExp tree, int spaces ) {
     indent( spaces );
-    System.out.println( "IntExp: " + tree.value );
+    System.out.println( "IntExp: " );
+  }
+
+  static public void showTree( CallExp tree, int spaces ) {
+    indent( spaces );
+    System.out.println( "CallExp:" );
   }
 
   static public void showTree( OpExp tree, int spaces ) {
@@ -98,9 +95,36 @@ abstract public class Absyn {
     showTree( tree.right, spaces );
   }
 
-  static public void showTree( VarExp tree, int spaces ) {
+  static public void showTree( AssignExp tree, int spaces ) {
     indent( spaces );
-    System.out.println( "VarExp: " + tree.name );
+    System.out.println( "AssignExp:" );
+    spaces += SPACES;
+    showTree( tree.lhs, spaces );
+    showTree( tree.rhs, spaces );
+  }
+
+  static public void showTree( IfExp tree, int spaces ) {
+    indent( spaces );
+    System.out.println( "IfExp:" );
+    spaces += SPACES;
+    showTree( tree.test, spaces );
+    showTree( tree.thenpart, spaces );
+    showTree( tree.elsepart, spaces );
+  }
+
+  static public void showTree( WhileExp tree, int spaces ) {
+    indent( spaces );
+    System.out.println( "WhileExp: " );
+  }
+
+  static public void showTree( ReturnExp tree, int spaces ) {
+    indent( spaces );
+    System.out.println( "ReturnExp: " );
+  }
+
+  static public void showTree( CompoundExp tree, int spaces ) {
+    indent( spaces );
+    System.out.println( "CompoundExp: "  );
   }
 
 }
