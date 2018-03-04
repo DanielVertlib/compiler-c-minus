c63e062897ead02f396c79d3a4783cd88ab1c47b
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 12:10:51 -0500

    added support for 19th grammar rule

--- absyn/VarExp.java
+++ absyn/VarExp.java
@@ -2,8 +2,10 @@ package absyn;
 
 public class VarExp extends Exp {
   public String name;
-  public VarExp( int pos, String name ) {
+  public Exp exp;
+  public VarExp( int pos, String name, Exp exp) {
     this.pos = pos;
     this.name = name;
+    this.exp = exp;
   }
 }
