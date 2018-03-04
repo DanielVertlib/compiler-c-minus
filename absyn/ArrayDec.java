53f31c8be12f4fa1156a1ef69d0b26b7fe0df704
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 13:26:08 -0500

    wrote and added ArrayDec function

---
+++ absyn/ArrayDec.java
@@ -0,0 +1,14 @@
+package abysn;
+
+public class ArrayDec extends VarDec {
+    public int pos;
+    public NameTy typ;
+    public String name;
+    public IntExp size;
+    public ArrayDec(int pos, NameTy typ, String name, IntExp size) {
+        this.pos = pos;
+        this.typ = typ;
+        this.name = name;
+        this.size = size;
+    }
+}
