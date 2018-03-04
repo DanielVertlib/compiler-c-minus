68f821a45d02eb6290e87fea29317c46510137e7
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 14:18:55 -0500

    added CompoundExp

---
+++ absyn/CompoundExp.java
@@ -0,0 +1,12 @@
+package absyn;
+
+public class CompoundExp extends Exp {
+    public int pos;
+    public VarDecList decs;
+    public ExpList exps;
+    public CompoundExp(int pos, VarDecList decs, ExpList exps)  {
+        this.pos = pos;
+        this.decs = decs;
+        this.exps = exps;
+    }
+}
