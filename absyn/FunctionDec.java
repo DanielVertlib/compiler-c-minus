e2ad869ed9c5631dd1b9425035f62bedd27f3424
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 14:29:43 -0500

    added FunctionDec

---
+++ absyn/FunctionDec.java
@@ -0,0 +1,15 @@
+package absyn;
+
+public class FunctionDec extends Dec {
+    public NameTy result;
+    public String func;
+    public VarDecList params;
+    public CompoundExp body;
+    public FunctionDec(int pos, NameTy result, String func, VarDecList params, CompoundExp body) {
+        this.pos = pos;
+        this.result = result;
+        this.func = func;
+        this.params = params;
+        this.body = body;
+    }
+}
