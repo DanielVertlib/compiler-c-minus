73352a8a03c94051b1b8b077dd456a599ef85e6a
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 13:41:08 -0500

    added missing ops to OpExp

--- absyn/OpExp.java
+++ absyn/OpExp.java
@@ -3,11 +3,14 @@ package absyn;
 public class OpExp extends Exp {
   public final static int PLUS  = 0;
   public final static int MINUS = 1;
-  public final static int TIMES = 2;
-  public final static int OVER  = 3;
+  public final static int MUL   = 2;
+  public final static int DIV   = 3;
   public final static int EQ    = 4;
-  public final static int LT    = 5;
-  public final static int GT    = 6;
+  public final static int NE    = 5;
+  public final static int LT    = 6;
+  public final static int LE    = 7;
+  public final static int GT    = 8;
+  public final static int GE    = 9;
 
   public Exp left;
   public int op;
