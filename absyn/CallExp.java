251e927c2d01f35bf60050292571c9036362ba27
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 13:06:17 -0500

    modified variable name in CallExp

--- absyn/CallExp.java
+++ absyn/CallExp.java
@@ -1,11 +1,11 @@
 package absyn;
 
 public class CallExp extends Exp {
-    public String id;
+    public String func;
     public ExpList args;
-    public CallExp(int pos, String id, ExpList args) {
+    public CallExp(int pos, String func, ExpList args) {
         this.pos = pos;
-        this.id = id;
+        this.func = func;
         this.args = args;
     }
 }
