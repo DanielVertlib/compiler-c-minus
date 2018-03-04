9de3dd1e9307c5e41e58295c13b28146c805dbe0
Ryan Rudzitis <ryanrudzitis@icloud.com>
2018-03-04 13:53:02 -0500

    added DecList and VarDecList files

---
+++ absyn/DecList.java
@@ -0,0 +1,10 @@
+package absyn;
+
+public class DecList {
+    public Dec head;
+    public DecList tail;
+    public DecList(Dec head, DecList tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+}
---
+++ absyn/VarDecList.java
@@ -0,0 +1,10 @@
+package absyn;
+
+public class VarDecList {
+    public VarDec head;
+    public VarDecList tail;
+    public VarDecList(VarDec head, VarDecList tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+}
